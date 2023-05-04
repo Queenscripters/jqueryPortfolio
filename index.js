1018619c6c308017bf17e103387132a2b61bbeca
Queen <queennefertitishabazz@gmail.com>
2023-05-03 18:56:51 -0700

    added jquery

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(
+    function(){
+        $(".header").animate({
+            backgroundColor:"aqua", 
+            left: "-200px",
+            marginLeft:"100px"});
+    });
+    $("h3").hover(function(){
+    
+    $("h3").mouseout(function(){
+      });
+  }
+  );
+  // $( document ).ready(function() {
+  //     console.log( "ready!" );
+  // });
